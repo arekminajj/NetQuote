3d052f852b7ed878f55a205403423aa327a6cbf1
ArekCios <arek123113@gmail.com>
2019-07-02 12:24:45 +0200

    Dodaj pliki projektów.

---
+++ NetQuote/Models/Quote.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetQuote.Models
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
